f6e3f6b8922d8f684441c1b03c175f7b85f95fa6
Goune <71518163+Goune1@users.noreply.github.com>
2024-06-04 22:53:21 +0200

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const nodemailer = require('nodemailer')
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+app.get('/', (req, res) => {
+  res.send("Welcome to the Macou2 api")
+})
+
+
+app.post('/api/macouEmail', async (req, res) => {
+    const data = req.body
+    console.log(data)
+
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'goune.ledreo@gmail.com', // Adresse e-mail Gmail
+            pass: 'ytij ijwl esem odwy ' // Mot de passe Gmail
+        }
+    });
+    
+    // Options du mail
+    const mailOptions = {
+        from: data.email,
+        to: 'ccdelmon@orange.fr',
+        subject: "Nouveau message venant du site de Macou",
+        text: `Monsieur/Madame ${data.firstName} ${data.lastName} vivant ${data.country} nous a contacté.\nSon adresse mail est : ${data.email} et son numéro de téléphone : ${data.phoneNumber}\nVoici son message :\n${data.message}`,
+    };
+    
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log('Erreur lors de l\'envoi de l\'e-mail :', error);
+        } else {
+            res.status(200).json({ message: "Correctly sent" });
+        }
+    })
+})
+
+
+
+// CONNEXION AU SERVEUR
+const PORT = process.env.PORT || 3001; // Utilise le port dynamique attribué par le fournisseur d'hébergement ou le port 3000 par défaut en local
+
+app.listen(PORT, () => {
+  console.log(`Le serveur écoute sur  http://localhost:${PORT}`);
+});
